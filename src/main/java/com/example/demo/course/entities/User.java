a7254cfe19c52760221400ab3f81aaff5e91c3e3
Luiz Gustavo <luizgustavo55779@gmail.com>
2019-11-08 18:58:47 -0300

    H2 database, test profile, JPA

--- src/main/java/com/example/demo/course/entities/User.java
+++ src/main/java/com/example/demo/course/entities/User.java
@@ -2,10 +2,17 @@ package com.example.demo.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //Anotação para dizer para o banco que é autoincrement
 	private Long id;
 	private String nome;
 	private String email;
