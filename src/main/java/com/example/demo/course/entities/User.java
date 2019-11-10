3f4005d8a83026f0d825457c3c5226f91ed0041c
Luiz Gustavo <luizgustavo55779@gmail.com>
2019-11-10 09:35:56 -0300

    Order, Instant, ISO 8601 - PART1

--- src/main/java/com/example/demo/course/entities/User.java
+++ src/main/java/com/example/demo/course/entities/User.java
@@ -1,54 +1,76 @@
 package com.example.demo.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) //Anotação para dizer para o banco que é autoincrement
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Anotação para dizer para o banco que é autoincrement
 	private Long id;
 	private String nome;
 	private String email;
 	private String telefone;
 	private String senha;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	public User(Long id, String nome, String email, String telefone, String senha) {
 		this.id = id;
 		this.nome = nome;
@@ -56,9 +78,11 @@ public class User implements Serializable {
 		this.telefone = telefone;
 		this.senha = senha;
 	}
+
 	public User() {
 
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -66,6 +90,7 @@ public class User implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -82,11 +107,11 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", nome=" + nome + ", email=" + email + ", telefone=" + telefone + ", senha=" + senha
 				+ "]";
 	}
-	
 
 }
